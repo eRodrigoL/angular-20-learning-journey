a9a4b67c07ee4121147adb56edb42842b5a6d305
E Rodrigo Lopes <159098656+eRodrigoL@users.noreply.github.com>
2025-11-25 11:41:01 -0300

    feat(calculadora): adiciona logica à `Calculadora`
    - define lógica interna em `calculadora/calculadora.ts`

--- conceitos-basicos/src/app/calculadora/calculadora.ts
+++ conceitos-basicos/src/app/calculadora/calculadora.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.scss',
 })
 export class Calculadora {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
 
+  calcularResultado() {
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
